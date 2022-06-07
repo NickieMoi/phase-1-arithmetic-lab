42cea9b40b566151fea91d1a8658e946a5c40e24
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-07 16:50:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1=31
+const num2=2
+const multiply =num1*num2;
 
-// Write your code here
+const random = Math.floor( Math.random() * 35 ) + 1;
+const num3 =24;
+const num4 =5;
+const mod = num3%num4
+let max = 20
+math.max (1,2,3,4,5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
